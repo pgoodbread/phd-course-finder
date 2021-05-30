e5ff5f7618a288898995da8d5b427259917d27d9
Ron Hansen <ron.hansen@axelspringer.com>
2021-05-30 18:08:21 +0200

    Implement coursetable in index page

---
+++ pages/api/courses/[id].ts
@@ -0,0 +1,78 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+import prisma from "../../../lib/prisma";
+import { createCourseValidation } from "../../../lib/validation";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === "PUT") {
+    return handleEdit(request, response);
+  }
+
+  if (request.method === "DELETE") {
+    return handleDelete(request, response);
+  }
+
+  return response.status(405).json("Method not allowed");
+}
+
+async function handleEdit(request: NextApiRequest, response: NextApiResponse) {
+  if (typeof request.query.id !== "string") {
+    return response.status(422).json("Url parameter not valid");
+  }
+
+  const body = JSON.parse(request.body);
+
+  const bodyIsValid = await createCourseValidation.isValid(body);
+  if (!bodyIsValid) {
+    return response.status(422).json("Form input not valid.");
+  }
+
+  const session = await getSession({ req: request });
+  if (!session) {
+    return response.status(401).json("Not authenticated");
+  }
+
+  await prisma.user.update({
+    where: { id: session.user.id },
+    data: {
+      courses: {
+        update: {
+          where: { id: request.query.id },
+          data: { ...body, date: new Date(body.date) },
+        },
+      },
+    },
+  });
+
+  return response.status(200).json(body);
+}
+
+async function handleDelete(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (typeof request.query.id !== "string") {
+    return response.status(422).json("Url parameter not valid");
+  }
+
+  const session = await getSession({ req: request });
+  if (!session) {
+    return response.status(401).json("Not authenticated");
+  }
+
+  await prisma.user.update({
+    where: { id: session.user.id },
+    data: {
+      courses: {
+        delete: {
+          id: request.query.id,
+        },
+      },
+    },
+  });
+
+  return response.status(200).json("");
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,59 @@
+import { InferGetStaticPropsType } from "next";
 import { signIn, signOut, useSession } from "next-auth/client";
+import Link from "next/link";
+import CourseTable from "../components/CourseTable";
+import prisma from "../lib/prisma";
 
-export default function Home() {
-  const [session, loading] = useSession();
+export default function Home({
+  courses,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [session] = useSession();
 
   return (
-    <>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user!.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
-    </>
+    <div className="flex flex-col">
+      <div className="flex justify-end my-4 mr-4">
+        {!session && (
+          <>
+            Not signed in <br />
+            <button
+              className="border-2 border-black px-4 rounded hover:bg-black hover:text-white ml-2"
+              onClick={() => signIn()}
+            >
+              Sign in
+            </button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button
+              className="border-2 border-black px-4 rounded hover:bg-black hover:text-white ml-2"
+              onClick={() => signOut()}
+            >
+              Sign out
+            </button>
+            <Link href="/courses">
+              <a className="border-2 border-black px-4 rounded hover:bg-black hover:text-white ml-2">
+                My Courses
+              </a>
+            </Link>
+          </>
+        )}
+      </div>
+
+      <CourseTable courses={courses} className="mt-4 mx-8" />
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      courses: await prisma.course.findMany({
+        orderBy: { date: "asc" },
+        where: { date: { gte: new Date() } },
+      }),
+    },
+    revalidate: 5,
+  };
 }
