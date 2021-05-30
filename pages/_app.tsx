612222f18bd2d179bcb53b8711400014c8529b42
Ron Hansen <ron.hansen@axelspringer.com>
2021-05-30 18:03:01 +0200

    Fix login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import "../styles/main.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
