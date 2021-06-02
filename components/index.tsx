b3598bdadf05af42a6417e71464c3f299ccdc557
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-06-02 22:40:40 +0200

    Add NavBar component

---
+++ components/index.tsx
@@ -0,0 +1,6 @@
+import CourseTable from "./CourseTable";
+import DeleteButton from "./DeleteButton";
+import PrimaryButton from "./PrimaryButton";
+import NavBar from "./NavBar";
+
+export { CourseTable, DeleteButton, NavBar, PrimaryButton };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,13 @@
 import { InferGetStaticPropsType } from "next";
-import { signIn, signOut, useSession } from "next-auth/client";
-import Link from "next/link";
-import CourseTable from "../components/CourseTable";
+import { CourseTable, NavBar } from "../components";
 import prisma from "../lib/prisma";
 
 export default function Home({
   courses,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [session] = useSession();
-
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-end my-4 mr-4">
-        {!session && (
-          <>
-            Not signed in <br />
-            <button
-              className="border-2 border-black px-4 rounded hover:bg-black hover:text-white ml-2"
-              onClick={() => signIn()}
-            >
-              Sign in
-            </button>
-          </>
-        )}
-        {session && (
-          <>
-            Signed in as {session.user.email} <br />
-            <button
-              className="border-2 border-black px-4 rounded hover:bg-black hover:text-white ml-2"
-              onClick={() => signOut()}
-            >
-              Sign out
-            </button>
-            <Link href="/courses">
-              <a className="border-2 border-black px-4 rounded hover:bg-black hover:text-white ml-2">
-                My Courses
-              </a>
-            </Link>
-          </>
-        )}
-      </div>
-
+    <div className="flex flex-col bg-gray-50">
+      <NavBar />
       <CourseTable courses={courses} className="mt-4 mx-8" />
     </div>
   );
