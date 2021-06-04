a283958887aa96d07b165ffd3410c1e5badc91e8
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-06-04 19:14:05 +0200

    add Footer

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,6 @@
 import CourseTable from "./CourseTable";
 import DeleteButton from "./DeleteButton";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
-export { CourseTable, DeleteButton, NavBar };
+export { CourseTable, DeleteButton, NavBar, Footer };
