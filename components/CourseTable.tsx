28bb54e9ceb50ca583dd294f77c249f0235a61aa
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-06-14 19:52:12 +0200

    make fee nullable

--- components/CourseTable.tsx
+++ components/CourseTable.tsx
@@ -203,7 +203,7 @@ export default function CourseTable({
                       </td>
 
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {course.credits}
+                        {course.credits !== null ? course.credits : "-"}
                       </td>
 
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
