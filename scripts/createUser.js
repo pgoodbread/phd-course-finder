4d4e2410befdab48c11f7beda8c601dfc4e1cd53
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-06-14 21:59:43 +0200

    Add createUser script

---
+++ scripts/createUser.js
@@ -0,0 +1,23 @@
+const { PrismaClient } = require(".prisma/client");
+const bcrypt = require("bcrypt");
+const prisma = new PrismaClient();
+
+const name = "test2";
+const password = "test2";
+const email = "test2@test2.de";
+
+async function createUser() {
+  const salt = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(password, salt);
+
+  await prisma.user.create({ data: { name, email, password: hash } });
+}
+
+createUser()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
