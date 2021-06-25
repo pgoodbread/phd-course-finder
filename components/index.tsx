bd5ed026d85617d3731788a8b31d9c62d65620bf
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-06-25 14:09:22 +0200

    Create FormInput component

--- components/index.tsx
+++ components/index.tsx
@@ -3,5 +3,6 @@ import DeleteButton from "./DeleteButton";
 import Footer from "./Footer";
 import Layout from "./Layout";
 import NavBar from "./NavBar";
+import FormInput from "./FormInput";
 
-export { CourseTable, DeleteButton, NavBar, Footer, Layout };
+export { CourseTable, DeleteButton, NavBar, Footer, Layout, FormInput };
