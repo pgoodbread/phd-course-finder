a062e6e566eceffc2e36650eb0a2fa481f383260
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-07-04 16:37:54 +0200

    finish basic form-styling

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -3,6 +3,7 @@ import { ErrorMessage, Field } from "formik";
 export default function FormInput({
   name,
   type,
+  className,
   label,
   placeholder,
   optional,
@@ -10,35 +11,14 @@ export default function FormInput({
 }: {
   name: string;
   type: string;
+  className?: string;
   label?: string;
   placeholder?: string;
   optional?: boolean;
   nullable?: boolean;
 }) {
-  function PriceInput() {
-    return (
-      <div>
-        <label
-          htmlFor="price"
-          className="block text-sm font-medium text-gray-700"
-        >
-          {label ? label : name}
-        </label>
-        <div className="mt-1 relative rounded-md shadow-sm">
-          <input
-            type="text"
-            name={name}
-            id="price"
-            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-            placeholder="0.00"
-          />
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className="mt-4">
+    <div className={`mt-4 px-2 ${className}`}>
       <div className="flex flex-row justify-between">
         <label
           htmlFor={name}
@@ -56,9 +36,9 @@ export default function FormInput({
             <span className="text-gray-500 sm:text-sm">€</span>
           </div>
         )}
-        {nullable ? (
+        {!nullable ? (
           <Field
-            className="focus:ring-primary focus:border-primary block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md text-primary"
+            className="focus:ring-primary focus:border-primary block w-full pl-3 pr-4 text-sm border-gray-300 rounded-md"
             type={type}
             name={name}
           />
@@ -66,7 +46,7 @@ export default function FormInput({
           <input
             type={type}
             name={name}
-            className="focus:ring-primary focus:border-primary block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+            className="focus:ring-primary focus:border-primary block w-full pl-3 pr-4 sm:text-sm border-gray-300 rounded-md"
             placeholder={placeholder && ""}
             // value={
             //   values.credits === null
@@ -76,8 +56,7 @@ export default function FormInput({
           />
         )}
       </div>
-      {/* replace name here */}
-      <ErrorMessage className="text-red-600" name="name" component="div" />
+      <ErrorMessage className="text-red-600" name={name} component="div" />
     </div>
   );
 }
