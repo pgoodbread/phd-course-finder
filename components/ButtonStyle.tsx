f025a6785cf9392c338a90f80ad948f43201c5bb
Ron Hansen <ron.hansen@axelspringer.com>
2021-09-06 19:15:14 +0200

    Fix react warning

--- components/ButtonStyle.tsx
+++ components/ButtonStyle.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 
-export default function ButtonStyle({
-  children,
-}: {
-  children: React.ReactElement;
-}) {
-  return (
-    <>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          className:
-            "w-full border border-primary bg-primary hover:text-primary hover:bg-white text-white px-4 py-2 rounded my-4 focus:outline-none " +
-            child.props.className,
-        });
-      })}
-    </>
-  );
-}
+const ButtonStyle = React.forwardRef(
+  (
+    {
+      children,
+    }: {
+      children: React.ReactElement;
+    },
+    ref
+  ) => {
+    return (
+      <>
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child, {
+            className:
+              "w-full border border-primary bg-primary hover:text-primary hover:bg-white text-white px-4 py-2 rounded my-4 focus:outline-none " +
+              child.props.className,
+            ref,
+          });
+        })}
+      </>
+    );
+  }
+);
+
+export default ButtonStyle;
