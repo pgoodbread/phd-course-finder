d10975449d89ded8b40a9c5f19bdd9c4b1d9084f
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-09-13 10:18:32 +0200

    add parking page for index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Layout } from "../components";
+import { useRouter } from "next/router";
 import "../styles/main.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  if (router.route === "/") {
+    return <Component {...pageProps} />;
+  }
   return (
     <Provider session={pageProps.session}>
       <Head>
