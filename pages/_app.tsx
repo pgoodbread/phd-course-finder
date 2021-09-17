01f32e3a471160fc5ffe9c3e8b3b640ef5f8223f
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-09-17 11:24:04 +0200

    add next-seo and basic seo configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,16 @@ import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Layout } from "../components";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 import "../styles/main.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
+      <DefaultSeo {...SEO} />
       <Head>
-        <title>CourseHub</title>
         <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Centralized platform for PhD-level courses. Spend less time searching, more time researching."
-        />
         <script
           defer
           data-domain="coursehub.app"
