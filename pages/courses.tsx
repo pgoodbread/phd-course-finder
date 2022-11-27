534309ecabf5b255d4ad980c5fd89ef0af2c70ef
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2022-11-27 11:25:21 +0100

    add expired courses

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -3,14 +3,19 @@ import { CourseTable } from "../components";
 import prisma from "../lib/prisma";
 
 export default function Home({
-  courses,
+  activeCourses,
+  previousCourses,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className="flex flex-col pt-4 md:pt-8">
       <h1 className="mt-4 mx-8 text-3xl text-gray-900 font-light">
-        All Courses
+        Future Courses
       </h1>
-      <CourseTable courses={courses} className="mt-4 mx-8" />
+      <CourseTable courses={activeCourses} className="mt-4 mx-8" />
+      <h1 className="mt-4 mx-8 text-3xl text-gray-900 font-light">
+        Expired Courses
+      </h1>
+      <CourseTable courses={previousCourses} className="mt-4 mx-8" />
     </div>
   );
 }
@@ -18,9 +23,13 @@ export default function Home({
 export async function getStaticProps() {
   return {
     props: {
-      courses: await prisma.course.findMany({
+      activeCourses: await prisma.course.findMany({
         orderBy: { start: "asc" },
         where: { start: { gte: new Date() }, deletedAt: null },
+      }),
+      previousCourses: await prisma.course.findMany({
+        orderBy: { start: "asc" },
+        where: { start: { lte: new Date() }, deletedAt: null },
       }),
     },
     revalidate: 5,
